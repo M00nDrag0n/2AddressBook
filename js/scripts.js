09375a30d66414810c7c212d3b67f871c082a200
Anise Lester and Judah Barton <anise+judah@>
2019-11-04 10:08:53 -0800

    add a working addressbook using objects

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+//  Business Logic for AdressBook ----------
+function AddressBook() {
+  this.contacts = [],
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {     
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+//  Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+var addressBook = new AddressBook();
+var contact = new Contact("Judah", "Barton", "111-111-1111");
+var contact2 = new Contact("Nuclear", "Flygon", "000-000-0000");
+addressBook.addContact(contact);
+addressBook.addContact(contact2);
+
+
+
+// // This is Practice, Anise
+// function Bugs(size, color, legAmount, type) {
+//   this.size = size;
+//   this.color = color;
+//   this.legAmount = legAmount;
+//   this.type = type;
+// };
+//
+//
+// Bugs.prototype.taxonomy = function() {
+//   return this.size + " " + this.color + " " + this.type;
+// };
+//
+// var leafhopper = new Bugs("tiny", "green", 6, "insect");
+// var blisterBeetle = new Bugs("medium", "red and back", 6, "insect");
+
+// in console, if you type the name of the variable (in this case leafhopper), add .(insert after "this" like so: leafhopper.color), it will return "green".
+// Ex: blisterBeetle.legAmount
+// --> 6
+// blisterBeetle.taxonomy();
+//  --> medium red and black insect
+// bugEncyclopedia.Bugs[0].type;
+//  [leafhopper] --> "insect"
+
+
+
+$(document).ready(function(){
+
+
+});
