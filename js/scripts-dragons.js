8b28378a55cc96cbb9e791c65af6e473a6409116
Anise Lester and Hristo Dzhambov <anise+hristo@>
2019-11-05 11:47:41 -0800

    add new project data to repository

---
+++ js/scripts-dragons.js
@@ -0,0 +1,29 @@
+
+function Vocab(word, definition) {
+  this.word = word;
+  this.definition = definition;
+};
+
+vocab.prototype.practice = function() {
+  return this.word + ": " + this.definition;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+});
--- js/scripts.js
+++ js/scripts.js
@@ -50,7 +50,16 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-var addressBook = new AddressBook();
+// var addressBook = new AddressBook(); --> don't put this at the top of the UIL section (or the screen in general, still trying to identify UIL vs BL sections), it will become a scary global variable which can mess up your code! (or something)
+function displayContactDetails(addressBookToDisplay) {
+  var contactList = $("ul#contacts");
+  var ForContactInfo = "";
+  addressBookDisplay.contact.forEach(function(contact){
+    forContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactList.html(forContactInfo);
+});
+
 var contact = new Contact("Judah", "Barton", "111-111-1111", "1111@example.com", "111 SE 1st Street");
 var contact2 = new Contact("Nuclear", "Flygon", "000-000-0000", "000@example.com", "000 SW Flying Fox Ave");
 var contact3 = new Contact("Hristo", "Smith", "4329900989", "898@example.com", "555 NE 33rd Ct");
@@ -68,10 +77,10 @@ var q2 = $("#q2").val();
 var q3 = $("#q3").val();
 var q4 = $("#q4").val();
 var q5 = $("#q5").val();
-var q6 = $("#q6").val();
+// var q6 = $("#q6").val();
 
 
 
-    $("#output").text(q1 + " " + q2 + " " + q3 + " " + q4 + " " + q5 + " " + q6);
+    $("#output").text(q1 + " " + q2 + " " + q3 + " " + q4 + " " + q5);
   });
 });
